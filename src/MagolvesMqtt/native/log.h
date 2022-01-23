60161fca2e35aff2b4accfeaca91376557d599e3
Oliver Wieland <oliver.wieland@unitybox.de>
2022-01-23 19:43:12 +0100

    Avoid warning on duplicate def

--- src/MagolvesMqtt/native/log.h
+++ src/MagolvesMqtt/native/log.h
@@ -8,11 +8,14 @@
 #ifndef LOG_H
 #define LOG_H
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 #define LOG_VERSION "0.1.0"
-#define LOG_USE_COLOR 
+
+#ifndef LOG_USE_COLOR
+#pragma message "LOG_USE_COLOR not defined - color support disabled"
+#endif
 
 typedef void (*log_LockFn)(void *udata, int lock);
 
@@ -20,8 +23,8 @@ enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
 #define log_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
 #define log_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
-#define log_info(...)  log_log(LOG_INFO,  __FILE__, __LINE__, __VA_ARGS__)
-#define log_warn(...)  log_log(LOG_WARN,  __FILE__, __LINE__, __VA_ARGS__)
+#define log_info(...) log_log(LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define log_warn(...) log_log(LOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
 #define log_error(...) log_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
 #define log_fatal(...) log_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
 
