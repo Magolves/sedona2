a3e50a10a6f0c1cc7a3e9b8563c9955358ace085
Oliver Wieland <oliver.wieland@unitybox.de>
2022-02-03 18:14:05 +0100

    Add render_is_valid_payload_length and render_payload_raw

--- src/vm-unix/mqtt_payload.c
+++ src/vm-unix/mqtt_payload.c
@@ -2,6 +2,9 @@
 #include "constants.h"
 #include "log.h"
 #include "sedona.h"
+#include <stdint.h>
+
+#define TYPE_ID_SIZE sizeof(uint16_t)
 
 size_t render_get_length(uint8_t *self, uint16_t offset, uint16_t tid) {
 
@@ -22,6 +25,64 @@ size_t render_get_length(uint8_t *self, uint16_t offset, uint16_t tid) {
     // const char *str = getInline(self, offset);
     return 0;
   }
+}
+
+bool render_is_valid_payload_length(size_t payload_len, uint8_t *self,
+                                    uint16_t offset, uint16_t tid) {
+  return render_get_length(self, offset, tid) + TYPE_ID_SIZE;
+}
+
+int render_payload_raw(uint8_t *buffer, size_t max_buf_len, uint8_t *self,
+                       uint16_t offset, uint16_t tid) {
+
+  size_t req_len = render_get_length(self, offset, tid);
+
+  if (max_buf_len < req_len) {
+    return -1;
+  }
+
+  switch (tid) {
+  case BoolTypeId:
+  case ByteTypeId:
+    // this line is required via C99 std; otherwise we get a compile error
+    // since every label mustfollowed by a statement and a
+    // decl is NOT a statement (sigh)
+    // We do not repeat ourselves, this is just language quirk
+    memcpy(buffer, &tid, TYPE_ID_SIZE);
+    buffer[TYPE_ID_SIZE] = getByte(self, offset);
+    break;
+  case ShortTypeId:
+    memcpy(buffer, &tid, TYPE_ID_SIZE);
+    uint16_t s = getShort(self, offset);
+    memcpy(buffer + TYPE_ID_SIZE, &s, req_len);
+    break;
+  case IntTypeId:
+    buffer[0] = tid;
+    int32_t i = getInt(self, offset);
+    memcpy(buffer + TYPE_ID_SIZE, &i, req_len);
+    break;
+  case FloatTypeId:
+    memcpy(buffer, &tid, TYPE_ID_SIZE);
+    float f = getFloat(self, offset);
+    memcpy(buffer + TYPE_ID_SIZE, &f, req_len);
+    break;
+  case DoubleTypeId:
+    memcpy(buffer, &tid, TYPE_ID_SIZE);
+    double d = (double)getWide(self, offset);
+    memcpy(buffer + TYPE_ID_SIZE, &d, req_len);
+    break;
+  case LongTypeId:
+    memcpy(buffer, &tid, TYPE_ID_SIZE);
+    long l = (long)getWide(self, offset);
+    memcpy(buffer + TYPE_ID_SIZE, &l, req_len);
+    break;
+  case BufTypeId:
+  default:
+    // NOT SUPPORTED
+    return -1;
+  }
+
+  return req_len;
 }
 
 void render_payload_json(char *buffer, size_t max_buf_len, uint8_t *self,
--- src/vm-unix/mqtt_payload.h
+++ src/vm-unix/mqtt_payload.h
@@ -2,6 +2,7 @@
 #define MQTT_PAYLOAD
 
 #include "sedona.h"
+#include <stdint.h>
 
 /// @brief Determines the payload size according to the type
 ///
@@ -10,6 +11,30 @@
 /// @param tid the Sedona type ID
 /// @return int the required amount of bytes to store the value
 size_t render_get_length(uint8_t *self, uint16_t offset, uint16_t tid);
+
+/// @brief Check if given payload has the proper length for a payload in
+/// our proprietary format.
+///
+/// @param payload_len the payload length of the (received) frame)
+/// @param self the component pointer
+/// @param offset the slot handle/offset
+/// @param tid the Sedona type ID
+/// @return true payload has valid length
+/// @return false payload is either too short or too long for this type
+bool render_is_valid_payload_length(size_t payload_len, uint8_t *self,
+                                    uint16_t offset, uint16_t tid);
+
+/// @brief Renders the given value into a proprietary raw format. The first word
+/// (HSB, LSB) is the Sedona type id and subsequent bytes are the raw bytes. The byte ordering
+/// depends on the machine.
+/// @param buffer the receiving byte buffer
+/// @param max_buf_len the maximum capacity of the buffer.
+/// @param self the component pointer
+/// @param offset the slot handle/offset
+/// @param tid the Sedona type ID
+/// @return int the number of copied bytes
+int render_payload_raw(uint8_t *buffer, size_t max_buf_len, uint8_t *self,
+                       uint16_t offset, uint16_t tid);
 
 /// @brief Renders the given slot value as JSON string. The JSON data is written
 /// into the buffer.
